87385b7d6706bd8b6ce413d1c8a8227b87c9ca14
leurjim <shaleao@icloud.com>
2022-03-28 11:10:09 -0500

    respuesta despues de enviar sus datos

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,18 +1,14 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import Layout from '../components/layout.js';
 import { para, inicio } from '../styles/thanks.module.css';
 
 export default function ThanksPage() {
   return (
-    <div>
-    <p className={para}>Gracias por contactarse con nosotros!!!</p>
-    <nav className={inicio}>
-      <ul>
-        <li>
-          <Link to="/">Inicio</Link>
-        </li>
-      </ul>
-    </nav>
-    </div>
+    <Layout>
+        <div>
+            <p className={para}>Gracias por contactarse con nosotros!!!</p>
+        </div>
+    </Layout>
   );
 }
