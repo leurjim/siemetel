e81a6b122bee14765845edb6c2ad48399479761f
leurjim <shaleao@icloud.com>
2022-03-28 06:54:26 -0500

    programando contactos

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,15 @@ import React from "react";
 import Layout from '../components/layout.js';
 import { content } from '../styles/contact.module.css';
 
-export default function ContactoPage() {
+export default function ContactPage() {
   return (
     <Layout>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" netlify>
   <p>
-    <label>Your Name: <input type="text" name="name" /></label>
+    <label>Name <input type="text" name="name" /></label>
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
+    <label>Email <input type="email" name="email" /></label>
   </p>
   <p>
     <button type="submit">Send</button>
