dee0843a3548219899d1a20e6a3d6fa00c008e6e
leurjim <shaleao@icloud.com>
2022-03-28 06:32:22 -0500

    programando contactos

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,27 +5,27 @@ import { content } from '../styles/contact.module.css';
 export default function ContactoPage() {
   return (
     <Layout>
-      <div className={content}>
-        
-          <h1>Contáctenos</h1>
-          <p>Raúl Arias / Leao Urbina</p>
-          <p>Celular: 922111168 / 923337297</p>
-          <p>Correos: gerencia.seimetel@gmail.com</p>
-        
-          <h1>Ingrese su información</h1>
-          <form
-      name="contact-form"
-      method="post"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input name="name" placeholder="Your Name" type="text" />
-      <input name="email" placeholder="name@name.com" type="email" />
-      <textarea name="message" />
-      <button>Send</button>
-    </form>
-       
-      </div>
+      <form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
     </Layout>
   );
 }
